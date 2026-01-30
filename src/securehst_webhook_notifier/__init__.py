56f34a335adde502cb3f7479f8ba4c45cc6fc52a
semantic-release <semantic-release>
2026-01-30 20:24:17 +0000

    1.1.0
    Automatically generated by python-semantic-release

--- src/securehst_webhook_notifier/__init__.py
+++ src/securehst_webhook_notifier/__init__.py
@@ -1,5 +1,5 @@
 from .main import notify_webhook, prefect_notify_webhook, send_webhook_message
 
-__version__ = "1.1.1"
+__version__ = "1.1.0"
 
 __all__ = ["notify_webhook", "prefect_notify_webhook", "send_webhook_message"]
